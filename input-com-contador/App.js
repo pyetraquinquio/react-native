066d0596792b6142c1cf6155bd51a7c8496aecb9
Pyetra <pyetraquinquio2007@gmail.com>
2024-05-28 17:28:34 -0300

    input e contador

---
+++ input-com-contador/App.js
@@ -0,0 +1,56 @@
+import React, {useState} from 'react';
+import { StyleSheet, View, Text, StatusBar, TextInput} from 'react-native';
+
+export default function App() {
+
+  const [campoNome, setCampoNome] = useState(""); 
+
+  
+  
+
+  return (
+    <View style={styles.container}>
+
+      <StatusBar backgroundColor="blueviolet" barStyle="light-content"/>
+
+      <TextInput
+      style={styles.input}
+      placeholder='Digite seu nome'
+      placeholderTextColor="blueviolet"
+      value={campoNome} //Define o valor atual do campo de entrada de texto como campoNome
+      onChangeText={(texto) => setCampoNome(texto)} //Atualiza o estado campoNome quando o texto do campo de entrada muda
+
+      />
+
+    <Text></Text>
+
+      <Text style={styles.texto}>Digitado no campo: {campoNome}</Text>
+
+      <Text style={styles.texto}>Quantidade de caracteres: {campoNome.length}</Text>
+
+      
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: "#e0b0ff"
+  },
+
+  texto: {
+    fontSize: 15
+  }, 
+
+  input: {
+    padding: 8,
+    borderWidth: 2,
+    borderRadius: 10,
+    width: 220,
+    backgroundColor: "#a87bc7"
+  }
+});
---
+++ input-com-contador/components/CoxinhaItemEstilo6.jsx
@@ -0,0 +1,37 @@
+import { View, Text, StyleSheet, Pressable } from 'react-native';
+
+
+export default function CoxinhaItemEstilo6(props) {
+ 
+   return (
+    <Pressable onPress={props.aoClicar}>
+      <View style={styles.conteiner}>
+        <Text style={styles.nome}>{props.nome}</Text>
+        <Text style={styles.preço}>{`R$: ${props.preço.toFixed(2)}`}</Text>
+        {props.originalPreço && props.originalPreço > props.preço && <Text style={styles.promo}>HOJE TEMOS PROMOÇÃO!!!</Text>}
+      </View>
+      </Pressable>
+    );
+  }
+
+const styles = StyleSheet.create({
+  conteiner: {
+    margin: 10,
+    padding: 10,
+    backgroundColor: "orange",
+  },
+
+  nome: {
+    fontSize: 17,
+    fontWeight: "bold",
+  },
+
+  preço: {
+    fontSize: 15,
+  },
+
+  promo: {
+    fontSize: 16,
+    fontWeight: "bold"
+  },
+})
